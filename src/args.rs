38f8bc6d1f959434116a6837c50836775bb50a10
Norbytus <norbyt93@gmail.com>
2020-02-19 20:51:15 +0300

    Move args validation in struct instead old variant

---
+++ src/args.rs
@@ -0,0 +1,80 @@
+use redis_module::RedisError;
+use regex::Regex;
+use std::convert::TryFrom;
+
+pub trait GetRegularExpression {
+    fn get_regular_expression(&self) -> &Regex;
+}
+
+fn get_regular_expression(raw_expression: &str) -> Result<Regex, RedisError> {
+    match Regex::new(&raw_expression) {
+        Ok(reg) => Ok(reg),
+        Err(e) => Err(RedisError::String(format!("{}", e))),
+    }
+}
+
+pub struct FindByKey {
+    regex: Regex,
+}
+
+impl GetRegularExpression for FindByKey {
+    fn get_regular_expression(&self) -> &Regex {
+        &self.regex
+    }
+}
+
+impl TryFrom<Vec<String>> for FindByKey {
+    type Error = RedisError;
+
+    fn try_from(args: Vec<String>) -> Result<Self, Self::Error> {
+        let args: Vec<String> = args.into_iter().skip(1).collect();
+
+        match args.get(0) {
+            Some(arg) => {
+                let regex = get_regular_expression(arg)?;
+                Ok(Self { regex })
+            }
+            None => Err(RedisError::Str("Not found first argument")),
+        }
+    }
+}
+
+pub struct FindByValue {
+    regex: Regex,
+    redis_mask: String,
+}
+
+impl FindByValue {
+    pub fn get_redis_mask(&self) -> &str {
+        &self.redis_mask
+    }
+}
+
+impl GetRegularExpression for FindByValue {
+    fn get_regular_expression(&self) -> &Regex {
+        &self.regex
+    }
+}
+
+impl TryFrom<Vec<String>> for FindByValue {
+    type Error = RedisError;
+
+    fn try_from(args: Vec<String>) -> Result<Self, Self::Error> {
+        let args: Vec<String> = args.into_iter().skip(1).collect();
+
+        let redis_mask = match args.get(0) {
+            Some(arg) => Ok(arg.to_string()),
+            None => Err(RedisError::Str("Not found first argument")),
+        }?;
+
+        let regex = match args.get(1) {
+            Some(arg) => get_regular_expression(arg),
+            None => Err(RedisError::Str("Not found second argument")),
+        }?;
+
+        Ok(Self {
+            regex: regex,
+            redis_mask: redis_mask,
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,15 @@
-use redis_module::{Context, RedisError, RedisResult, redis_module, redis_command, RedisValue};
+use crate::args::GetRegularExpression;
+use redis_module::{redis_command, redis_module, Context, RedisError, RedisResult, RedisValue};
 use regex::Regex;
+use std::convert::TryFrom;
+
+pub mod args;
 
 const REDIS_COMMAND_KEYS: &'static str = "KEYS";
 
 const REDIS_COMMAND_MGET: &'static str = "MGET";
 
-fn get_regular_expression(raw_expression: &str) -> Result<Regex, RedisError> {
-    match Regex::new(&raw_expression) {
-        Ok(reg) => Ok(reg),
-        Err(e) => Err(RedisError::String(format!("{}", e)))
-    }
-}
-
-fn handle_redis_command_result(result: Vec<RedisValue>)
-    -> impl Iterator<Item = String>
-{
+fn handle_redis_command_result(result: Vec<RedisValue>) -> impl Iterator<Item = String> {
     result
         .into_iter()
         .map(|value| match value {
@@ -25,29 +20,20 @@ fn handle_redis_command_result(result: Vec<RedisValue>)
         .map(|value| value.unwrap())
 }
 
-
 fn find_keys_by_rg(ctx: &Context, args: Vec<String>) -> RedisResult {
-    let need_arg: Vec<String> = args.into_iter().skip(1).map(|s| s).collect();
-
-    let first_arg: &str = match need_arg.get(0) {
-        Some(data) => Ok(data),
-        None => {
-            ctx.log_debug("Not found regular expression arguments value");
-            Err(RedisError::WrongArity)
-        }
-    }?;
-
-    let reg = get_regular_expression(first_arg)?;
+    let args = crate::args::FindByKey::try_from(args)?;
 
     let response_from_command = ctx.call(REDIS_COMMAND_KEYS, &["*"])?;
 
     let result = match response_from_command {
         RedisValue::Array(data) => Ok(data),
-        _ => Err(RedisError::Str("Wrong return result from `KEYS` command, expected array.")),
+        _ => Err(RedisError::Str(
+            "Wrong return result from `KEYS` command, expected array.",
+        )),
     }?;
 
     let result_redis_value: Vec<RedisValue> = handle_redis_command_result(result)
-        .filter(move |s| reg.is_match(&s))
+        .filter(move |s| args.get_regular_expression().is_match(&s))
         .map(|s| RedisValue::SimpleString(s))
         .collect();
 
@@ -59,46 +45,36 @@ fn find_keys_by_rg(ctx: &Context, args: Vec<String>) -> RedisResult {
 }
 
 fn find_values_by_rg(ctx: &Context, args: Vec<String>) -> RedisResult {
-    let args: Vec<String> = args.into_iter().skip(1).collect();
+    let args = crate::args::FindByValue::try_from(args)?;
 
-    if args.len() < 2 {
-        return Err(RedisError::WrongArity);
-    }
-
-    let key = match args.get(0) {
-        Some(key) => Ok(key),
-        None => Err(RedisError::Str("First argument must data key")),
-    }?;
-
-    let raw_reg = match args.get(1) {
-        Some(reg) => Ok(reg),
-        None => Err(RedisError::Str("Second argument must regular expression")),
-    }?;
-
-    let reg = get_regular_expression(raw_reg)?;
-
-    let response_from_keys_command = ctx.call(REDIS_COMMAND_KEYS, &[key])?;
+    let response_from_keys_command = ctx.call(REDIS_COMMAND_KEYS, &[args.get_redis_mask()])?;
 
     let result_keys_command = match response_from_keys_command {
         RedisValue::Array(data) => Ok(data),
-        _ => Err(RedisError::Str("Wrong return result from `MGET` command, expected array.")),
+        _ => Err(RedisError::Str(
+            "Wrong return result from `MGET` command, expected array.",
+        )),
     }?;
 
-    let result_redis_keys: Vec<String> = handle_redis_command_result(result_keys_command)
-        .collect();
+    let result_redis_keys: Vec<String> = handle_redis_command_result(result_keys_command).collect();
 
     let response_from_mget_command = ctx.call(
         REDIS_COMMAND_MGET,
-        &result_redis_keys.iter().map(|k| k.as_ref()).collect::<Vec<&str>>()
+        &result_redis_keys
+            .iter()
+            .map(|k| k.as_ref())
+            .collect::<Vec<&str>>(),
     )?;
 
     let result_from_mget_command = match response_from_mget_command {
         RedisValue::Array(data) => Ok(data),
-        _ => Err(RedisError::Str("Wrong return result from `KEYS` command, expected array.")),
+        _ => Err(RedisError::Str(
+            "Wrong return result from `KEYS` command, expected array.",
+        )),
     }?;
 
     let result_redis_value: Vec<RedisValue> = handle_redis_command_result(result_from_mget_command)
-        .filter(move |s| reg.is_match(&s))
+        .filter(move |s| args.get_regular_expression().is_match(&s))
         .map(|s| RedisValue::SimpleString(s))
         .collect();
 
@@ -109,7 +85,7 @@ fn find_values_by_rg(ctx: &Context, args: Vec<String>) -> RedisResult {
     }
 }
 
-redis_module!{
+redis_module! {
     name: "Regex",
     version: 0.1,
     data_types: [],
