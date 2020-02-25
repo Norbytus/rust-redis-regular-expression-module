129c7bdf0bece0eb3be1a5f73c50ed6cdaf445a2
Norbytus <norbyt93@gmail.com>
2020-02-25 19:54:06 +0300

    Return array keys instead array values in rgvalue command, and use open_key instead MGET redis command

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 use crate::args::GetRegularExpression;
-use redis_module::{redis_command, redis_module, Context, RedisError, RedisResult, RedisValue};
+use redis_module::{
+    redis_command,
+    redis_module,
+    Context,
+    RedisError,
+    RedisResult,
+    RedisValue
+};
 use std::convert::TryFrom;
 
 pub mod args;
 
 const REDIS_COMMAND_KEYS: &'static str = "KEYS";
-
-const REDIS_COMMAND_MGET: &'static str = "MGET";
 
 fn handle_redis_command_result(result: Vec<RedisValue>) -> impl Iterator<Item = String> {
     result
@@ -46,7 +51,10 @@ fn find_keys_by_rg(ctx: &Context, args: Vec<String>) -> RedisResult {
 fn find_values_by_rg(ctx: &Context, args: Vec<String>) -> RedisResult {
     let args = crate::args::FindByValue::try_from(args)?;
 
-    let response_from_keys_command = ctx.call(REDIS_COMMAND_KEYS, &[args.get_redis_mask()])?;
+    let response_from_keys_command = ctx.call(
+        REDIS_COMMAND_KEYS,
+        &[args.get_redis_mask()]
+    )?;
 
     let result_keys_command = match response_from_keys_command {
         RedisValue::Array(data) => Ok(data),
@@ -55,26 +63,21 @@ fn find_values_by_rg(ctx: &Context, args: Vec<String>) -> RedisResult {
         )),
     }?;
 
-    let result_redis_keys: Vec<String> = handle_redis_command_result(result_keys_command).collect();
-
-    let response_from_mget_command = ctx.call(
-        REDIS_COMMAND_MGET,
-        &result_redis_keys
-            .iter()
-            .map(|k| k.as_ref())
-            .collect::<Vec<&str>>(),
-    )?;
-
-    let result_from_mget_command = match response_from_mget_command {
-        RedisValue::Array(data) => Ok(data),
-        _ => Err(RedisError::Str(
-            "Wrong return result from `KEYS` command, expected array.",
-        )),
-    }?;
-
-    let result_redis_value: Vec<RedisValue> = handle_redis_command_result(result_from_mget_command)
-        .filter(move |s| args.get_regular_expression().is_match(&s))
-        .map(|s| RedisValue::SimpleString(s))
+    let result_redis_value: Vec<RedisValue> = handle_redis_command_result(result_keys_command)
+        .map(move |key| {
+            let redis_key = ctx.open_key(&key);
+            match redis_key.read() {
+                Ok(value) => (Some(key), value),
+                Err(e) => {
+                    ctx.log_debug(&format!("{}", e));
+                    (None, None)
+                }
+            }
+        })
+        .filter(|(key, value)| value.is_some() && key.is_some())
+        .map(|(key, value)| (key.unwrap(), value.unwrap()))
+        .filter(move |(_, value)| args.get_regular_expression().is_match(&value))
+        .map(|(key, _)| RedisValue::SimpleString(key))
         .collect();
 
     if result_redis_value.is_empty() {
